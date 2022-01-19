25862c550a796909de812bce1affec0943210115
CatLuck2 <freeaddress834@gmail.com>
2022-01-19 17:35:57 +0900

    MemoListViewController（.storyboard）でTableViewCellを削除、MemoListTableViewCellを新規作成

---
+++ SakuMemo/MemoListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MemoListTableViewCell.swift
+//  SakuMemo
+//
+//  Created by Nekokichi on 2022/01/19.
+//
+
+import UIKit
+
+class MemoListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

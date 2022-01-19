8038bb674ff7d2458611ea9350caf20d1899979d
CatLuck2 <freeaddress834@gmail.com>
2022-01-19 17:33:31 +0900

    メモのタイトルとテキストを持つ、MemoModel.swiftを新規作成

--- SakuMemo/MemoListViewController.swift
+++ SakuMemo/MemoListViewController.swift
@@ -18,4 +18,3 @@ class MemoListViewController: UIViewController {
 
 
 }
-
---
+++ SakuMemo/MemoModel.swift
@@ -0,0 +1,13 @@
+//
+//  MemoModel.swift
+//  SakuMemo
+//
+//  Created by Nekokichi on 2022/01/19.
+//
+
+import UIKit
+
+struct MemoModel {
+    private var title: String
+    private var sentence: String
+}

49673a88d34355d4a0d9cda4b7e6564a2add0a2f
CatLuck2 <freeaddress834@gmail.com>
2022-01-19 17:54:28 +0900

    MemoListViewControllerでtableViewのdelegate/dataSourceのメソッドを追加、Main.StoryboardでMemoEditViewControllerにStoryboardIDを設定

--- SakuMemo/MemoListViewController.swift
+++ SakuMemo/MemoListViewController.swift
@@ -13,8 +13,33 @@ class MemoListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        listTableView.delegate = self
+        listTableView.dataSource = self
     }
 
 
+}
+
+extension MemoListViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = UITableViewCell(style: .default, reuseIdentifier: "MemoListTableViewCellID") as? MemoListTableViewCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let memoEditViewController = self.storyboard?.instantiateViewController(withIdentifier: "segueToMemoEditViewController") as? MemoEditViewControlelr else {
+            return
+        }
+        self.navigationController?.pushViewController(memoEditViewController, animated: true)
+    }
 }

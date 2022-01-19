e61228c88d200195738a5a8eaff71f5c1de451b9
CatLuck2 <freeaddress834@gmail.com>
2022-01-19 16:39:17 +0900

    ViewController.swiftを解明、新規ViewController.swiftを作成、StoryboardでViewControllerを新規追加

---
+++ SakuMemo/MemoEditViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MemoEditViewController.swift
+//  SakuMemo
+//
+//  Created by Nekokichi on 2022/01/19.
+//
+
+import UIKit
+
+class MemoEditViewControlelr: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
